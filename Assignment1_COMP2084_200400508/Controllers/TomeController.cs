88995e85368068586f8d3e87c494d3ae3c93f233
Adam Stephens <200400508@student.georgianc.on.ca>
2019-10-16 15:51:05 -0400

    Added SpellController and TomeController, as well as modified the connection string in appsettings.json Had to go do something, so this is all for now.

---
+++ Assignment1_COMP2084_200400508/Controllers/TomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment1_COMP2084_200400508.Controllers
+{
+    public class TomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
