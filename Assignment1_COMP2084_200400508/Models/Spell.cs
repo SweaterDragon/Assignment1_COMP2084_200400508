49e98d5aae2a79b98d8cc0136c688585d5024303
Adam Stephens <200400508@student.georgianc.on.ca>
2019-10-16 15:12:59 -0400

    Added the Tome and Spell classes. The theme of the application will be a library for wizards

---
+++ Assignment1_COMP2084_200400508/Models/Spell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1_COMP2084_200400508.Models
+{
+    public class Spell
+    {
+        [Key]
+        public int SpellId { get; set; }
+
+        [ForeignKey("Tome")]
+        public int TomeId { get; set; }
+
+        public string Name { get; set; }
+        // Art refers to what school of magic, such as necromancy, pyromancy, or restoration
+        public string Art { get; set; }
+
+        public int Difficulty { get; set; }
+    }
+}
---
+++ Assignment1_COMP2084_200400508/Models/Tome.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1_COMP2084_200400508.Models
+{
+    public class Tome
+    {
+        [Key]
+        public int TomeId { get; set; }
+
+        public string Name { get; set; }
+        
+        public int Pages { get; set; }
+
+        public string Description { get; set; }
+
+        public Boolean IsRented { get; set; }
+
+        public ICollection<Spell> Spells { get; set; }
+
+    }
+}
